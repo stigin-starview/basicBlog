39273590883280fdd9651f1f2e9bef4f68520a8e
stigin starview <stiginstarview@hotmail.com>
2020-12-16 13:51:30 +0530

    implimenting ui with bootstrap and used django forms for user registration

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (
+    home,
+    about
+)
+
+
+urlpatterns = [
+    path('', home, name='blog-home'),
+    path('about/', about, name='blog-about'),
+]
